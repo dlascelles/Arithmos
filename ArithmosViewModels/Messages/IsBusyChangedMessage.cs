f3104de1479bc2f2878e0ff96f86e775801f60f3
Daniel Lascelles <lascelles@protonmail.com>
2021-07-12 10:39:43 +0300

    Added new message IsBusyChangedMessage

---
+++ ArithmosViewModels/Messages/IsBusyChangedMessage.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Toolkit.Mvvm.Messaging.Messages;
+
+namespace ArithmosViewModels.Messages
+{
+    public class IsBusyChangedMessage : ValueChangedMessage<bool>
+    {
+        public IsBusyChangedMessage(bool isBusy) : base(isBusy) { }
+    }
+}
